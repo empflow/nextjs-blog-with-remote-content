16d2bad0865729690a5ec26f20a427a43fc67331
Matvey <morozovmatvej75@gmail.com>
2023-06-24 17:48:39 +0300

    create new project

--- src/app/api/echo/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET(req: Request) {
-  const searchParamsEntries = new URLSearchParams(req.url).entries();
-  const searchParamsObj = Object.fromEntries(searchParamsEntries);
-  return NextResponse.json(searchParamsObj);
-}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      boxShadow: {
-        "input-outline": "0 0 0 2px rgba(0,0,0,0)",
-        "button-outline": "0 0 0 1px #fff, 0 0 0 3px #3b82f6",
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
   plugins: [],
-};
+}
