97f63bd4762cf6ee2245fbf38fc62ec71cdfbec6
Matvey <morozovmatvej75@gmail.com>
2023-06-24 18:26:26 +0300

    get all todos on /api/todos

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -3,18 +3,8 @@ import axios from "axios";
 
 const DATA_SOURCE_URL = "https://jsonplaceholder.typicode.com/todos";
 
-export async function GET(req: Request) {
-  const url = new URL(req.url);
-  const todoId = new URLSearchParams(url.searchParams).get("id");
-  if (!todoId) return NextResponse.json({ message: "No todo ID provided" });
-
-  try {
-    const res = await axios.get(`${DATA_SOURCE_URL}/${todoId}`);
-    return NextResponse.json(res.data);
-  } catch (err) {
-    console.log(err);
-    return NextResponse.json(
-      ((err as any).message as string | undefined) ?? err
-    );
-  }
+export async function GET() {
+  const res = await axios.get(DATA_SOURCE_URL);
+  const todos: Todo[] = res.data;
+  return NextResponse.json(todos);
 }
