daf27fd15ba1cdd00749ee9de525de5cce806426
Matvey <morozovmatvej75@gmail.com>
2023-06-25 14:39:40 +0300

    do nothing in the mw

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(req: NextRequest) {
-  const regex = new RegExp("/api/*");
-  if (regex.test(req.url)) {
-    console.log("matches");
-  }
-  console.log(req.method);
-  console.log(req.url);
-  const origin = req.headers.get("origin");
-  if (origin) console.log(origin);
-
   return NextResponse.next();
 }
