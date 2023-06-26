d5bd2f40f574eaabd6f9ef8f7e9c49a3194c0e53
Matvey <morozovmatvej75@gmail.com>
2023-06-26 11:26:57 +0300

    change styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,26 @@
-import './globals.css'
-import Navbar from './components/Navbar'
-import MyProfilePic from './components/MyProfilePic'
+import "./globals.css";
+import Navbar from "./components/Navbar";
+import { Metadata } from "next";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Dave's Blog",
-  description: 'Created by Dave Gray',
-}
+  description: "Created by Dave Gray",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className="dark:bg-slate-800">
-        <Navbar />
-        <MyProfilePic />
-        {children}
+        <main className="m-auto px-4 md:px-6 prose prose-xl prose-slate dark:prose-invert mx-auto">
+          <Navbar />
+
+          {children}
+        </main>
       </body>
     </html>
-  )
+  );
 }
