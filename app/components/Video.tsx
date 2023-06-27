5eb848623c462e9389989ddfd66aaacfc491cf99
Matvey <morozovmatvej75@gmail.com>
2023-06-27 10:33:03 +0300

    give rounded corners to video player

--- app/components/Video.tsx
+++ app/components/Video.tsx
@@ -9,6 +9,7 @@ export default function Video({ id }: Props) {
         src={`https://www.youtube-nocookie.com/embed/${id}`}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        className="rounded-md"
       ></iframe>
     </div>
   );
